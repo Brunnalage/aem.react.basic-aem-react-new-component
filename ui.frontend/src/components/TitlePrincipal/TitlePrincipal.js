23cfcc3a986e11aa06e772e37984b29251a02b49
Brunna Lage <brunnalage@gmail.com>
2022-07-26 18:50:58 -0300

    Refactor: removing styling with styled components

--- ui.frontend/src/components/TitlePrincipal/TitlePrincipal.js
+++ ui.frontend/src/components/TitlePrincipal/TitlePrincipal.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {MapTo} from '@adobe/aem-react-editable-components';
 import { TitlePrincipalStyled } from './TitlePrincipal.styled';
+import './TitlePrincipal.css'
 
 const TitlePrincipal = (props) => {
     return(
-        <TitlePrincipalStyled>{props.text}</TitlePrincipalStyled>
+        <h2 className='principal'>{props.text}</h2>
     )
 }
 

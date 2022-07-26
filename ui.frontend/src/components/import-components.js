63a339291d6360f1c2bd43878770495278abcf93
Brunna Lage <brunnalage@gmail.com>
2022-07-26 17:27:45 -0300

    Feat: importing the TitlePrincipal component in the component import file

--- ui.frontend/src/components/import-components.js
+++ ui.frontend/src/components/import-components.js
@@ -19,6 +19,7 @@ import "./Page/Page";
 import "./Container/Container";
 import "./ExperienceFragment/ExperienceFragment";
 import "./XxXxx";
+import "./TitlePrincipal/TitlePrincipal";
 import MeuComponente from "./MeuComponente";
 
 import { MapTo } from "@adobe/aem-react-editable-components";

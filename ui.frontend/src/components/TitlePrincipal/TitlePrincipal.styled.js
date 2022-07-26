a7499a4bd9063195175c9534f1361633ab787b93
Brunna Lage <brunnalage@gmail.com>
2022-07-26 17:26:28 -0300

    Style: TitlePrincipal component styles creation

---
+++ ui.frontend/src/components/TitlePrincipal/TitlePrincipal.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const TitlePrincipalStyled = styled.h2`
+font-family: 'Space Mono';
+font-style: normal;
+font-weight: 700;
+font-size: 4rem;
+line-height: 5.938rem;
+letter-spacing: -0.035em;
+color: #333333;
+text-align:left;
+max-width: 15ch;
+`;

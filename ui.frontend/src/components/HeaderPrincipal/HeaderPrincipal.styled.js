f147c5350d4a5dc0997de91a92d8494fa2b8ba3b
Brunna Lage <brunnalage@gmail.com>
2022-07-27 18:41:12 -0300

    Style: creating styles for the HeaderPrincipal component

---
+++ ui.frontend/src/components/HeaderPrincipal/HeaderPrincipal.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+margin-bottom: 8.888rem;
+`;
+
+export const HeaderPrincipalStyled = styled.h3`
+font-family: 'Inconsolata', monospace !important;
+font-style: normal;
+font-weight: 700;
+font-size: 1.5rem;
+line-height: 1.563rem;
+letter-spacing: -0.08em;
+text-transform: uppercase;
+
+color: #333333;
+`;

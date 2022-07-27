b8dfc5d4976d50878cf53d2df80d3daf3c3199e4
Brunna Lage <brunnalage@gmail.com>
2022-07-27 14:08:39 -0300

    Style: text and div size adjustments

--- ui.frontend/src/components/ParagraphPrincipal/ParagraphPrincipal.styled.js
+++ ui.frontend/src/components/ParagraphPrincipal/ParagraphPrincipal.styled.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const PContainer = styled.div`
-max-width: 23.813rem;
+max-width: 16.931rem;
 `;
 
 export const PPrincipalStyled = styled.p`
  font-family: 'Space Mono', monospace !important;
     font-style: normal;
     font-weight: 400;
-    font-size: 1.5rem;
-    line-height: 2.25rem;
+    font-size: 1.063rem;
+    line-height: 1.6rem;
     letter-spacing: -0.035em;
     color: #4F4F4F;
     

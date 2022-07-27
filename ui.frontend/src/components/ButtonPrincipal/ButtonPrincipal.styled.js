b56ed8fe41d468353ae7fd974edc695c53a61024
Brunna Lage <brunnalage@gmail.com>
2022-07-27 15:23:38 -0300

    Style: creating the Button component styles

---
+++ ui.frontend/src/components/ButtonPrincipal/ButtonPrincipal.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const BtnPrincipalStyled = styled.button`
+height: 3.019rem;
+background: #333333;
+font-family: 'Space Mono', monospace !important;
+font-style: normal;
+font-weight: 700;
+color:#FFFFFF;
+font-size: 0.625rem;
+line-height: 0.938rem;
+letter-spacing: -0.035em;
+text-transform: uppercase;
+text-align: center;
+border: none;
+padding: 1.063rem 1.906rem;
+`;

89949e2a07fe4b7901599488dd43b22cf6e1e5df
Brunna Lage <brunnalage@gmail.com>
2022-07-27 20:14:35 -0300

    Style:  creating footer component styles

---
+++ ui.frontend/src/components/FooterPrincipal/FooterPrincipal.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const FooterPrincipalContainer = styled.div`
+margin-top: 19.25rem;
+align-items: center;
+display: flex;
+justify-content: center;
+gap: 5px;
+`;
+
+export const TextFooter = styled.span`
+font-family: 'Montserrat', sans-serif !important;
+font-style: normal;
+font-weight: 500;
+font-size: 0.875rem;
+line-height: 1.063rem;
+text-align: center;
+color: #BDBDBD;
+`;
+
+export const UserNameFooter = styled.span`
+font-family: 'Montserrat', sans-serif !important;
+font-style: normal;
+font-weight: 700;
+font-size: 0.875rem;
+line-height: 1.063rem;
+text-align: center;
+color: #BDBDBD;
+text-decoration: underline;
+`;

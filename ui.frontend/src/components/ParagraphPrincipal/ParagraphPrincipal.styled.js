511861db52a8210eb78ad162d3eac101acead575
Brunna Lage <brunnalage@gmail.com>
2022-07-26 23:26:35 -0300

    Style: creating the styles of the paragraph component

---
+++ ui.frontend/src/components/ParagraphPrincipal/ParagraphPrincipal.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PContainer = styled.div`
+max-width: 23.813rem;
+`;
+
+export const PPrincipalStyled = styled.p`
+ font-family: 'Space Mono', monospace !important;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1.5rem;
+    line-height: 2.25rem;
+    letter-spacing: -0.035em;
+    color: #4F4F4F;
+    
+`;
